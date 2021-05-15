bef74d10b028c195743534786314982aa1545cf8
Hazelnut milk tea <82927154+JenGibhub@users.noreply.github.com>
2021-05-15 23:48:41 +1000

    adminpage style and table style change

--- adminpage.php
+++ adminpage.php
@@ -6,18 +6,11 @@ session_start();
 <html>
 <head>
 <title>Admin Page</title>
-<style>
-table, th, td {
-  border: 1px solid black;
-}
-</style>
+<link rel="stylesheet" href="style/styles.css" />
 </head>
 <body>
 
-    <div style="float: right;">
-        <button type="button" onclick="location.href='logout.php'">Logout</button>
-    </div>
-
+    <div class="side-bar">
     <?php
     
         
@@ -32,14 +25,17 @@ table, th, td {
         echo "You are not logged in";
         exit;
     }
-?>
+    ?>
 
-   
+        <img src="/images/medicine.png" alt="logo" />
+        <div class="side-title">PHP-SRePS</div>
+        <button type="button" onclick="location.href='adduser.php'">Click Here to add a User!</button>
+        <button type="button" onclick="location.href='logout.php'">Logout</button>
+    </div>
 
-    <div>
-    <button type="button" onclick="location.href='adduser.php'">Click Here to add a User!</button>
-    
-   <form action="search_user_process.php" methord="get">
+
+    <div class="main">
+    <form action="search_user_process.php" methord="get">
        <lable>Enter User ID here :</lable>
        <input type="text" name="user_ID"/>
 
@@ -49,53 +45,58 @@ table, th, td {
        or
        <button type="submit" name="update" formaction="update_user.php">Update User!</button>
    </form>
+
    
    </div>
 
 
 
-    <table style="width:100%" >
-    <tr>
-        <th>idUSERS</th>
-        <th>User Name</th>
-        <th>Password</th>
-        <th>admin</th>
-    </tr>
-    <?php
-        require_once 'userDTO.php';
-        $userdto = new userDTO(null,null,null);
-        $info_holder = $userdto->displayall();
+    <table>
+        <thead>
+            <tr>
+                <th>idUSERS</th>
+                <th>User Name</th>
+                <th>Password</th>
+                <th>admin</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                require_once 'userDTO.php';
+                $userdto = new userDTO(null,null,null);
+                $info_holder = $userdto->displayall();
 
-        $ID_array = $info_holder[0];
-        $username_array = $info_holder[1];
-        $password_array = $info_holder[2];
-        $admin_array = $info_holder[3];
-        
-
-
-        $x = 0;
-        foreach($ID_array as $value)
-        {
-            echo "<tr>
-
-            <th>{$ID_array[$x]}</th>
-            <th>{$username_array[$x]}</th>
-            <th>{$password_array[$x]}</th>
-            <th>{$admin_array[$x]}</th>
-
-            </tr>";
-
-            $x++;
-        }
-    
+                $ID_array = $info_holder[0];
+                $username_array = $info_holder[1];
+                $password_array = $info_holder[2];
+                $admin_array = $info_holder[3];
+                
 
 
-    ?>
+                $x = 0;
+                foreach($ID_array as $value)
+                {
+                    echo "<tr>
 
-</table>
+                    <th>{$ID_array[$x]}</th>
+                    <th>{$username_array[$x]}</th>
+                    <th>{$password_array[$x]}</th>
+                    <th>{$admin_array[$x]}</th>
+
+                    </tr>";
+
+                    $x++;
+                }
+            
+
+
+            ?>
+        </tbody>
+
+    </table>
 
     <button type="button" onclick="location.href='recordspage.php'">Go back</button>
-
+    </div>
 
 
 
