6c91dc3555a2c2b55af7f7094c94002077740c7d
Inab <yolo.yyshi@yahoo.com>
2021-05-15 22:09:49 +1000

    remember login when return homepage

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,15 +18,40 @@
             </div>
             
             <div class="right">
-                <div class="form">
-                    <form action="loginprocess.php" method="POST">
-                        <label class="label">User Name</label>
-                        <input type="text" name="username" class="input-text" />
-                        <label class="label">Password</label>
-                        <input type="text" name="password" class="input-text" />
-                        <div class="button-row"><button type="submit" name="submit" class="submit">Login</button></div>
-                    </form>
-                </div>
+                <?php
+                    if(isset($_SESSION["user_name"]))
+                    {
+                        $user = $_SESSION["user_name"];
+                        if($_SESSION['admin_check'] == "true")
+                            echo "<div class='msg sucmsg'>Welcome $user</div>";
+                        else
+                            echo "<div class='msg sucmsg'>Guest Login</div>";
+                        
+                        echo "<div class='button-row'><button type=\"button\" onclick=\"location.href='recordspage.php'\" class='button btn2'>Access Records</button></div>";
+                            
+                        if($_SESSION['admin_check'] == "true")
+                        {
+                            echo "<div class='button-row next-row'><button type=\"button\" onclick=\"location.href='adminpage.php'\" class='button btn2'>Admin Page</button></div>";
+                        }
+                            
+                        echo "<div class='button-row next-row'><button type=\"button\" onclick=\"location.href='logout.php'\" class='button btn2'>Logout</button></div>";
+
+                    }
+                    else
+                    {
+                        echo "<div class='form'>
+                            <form action='loginprocess.php' method='POST'>
+                                <label class='label'>User Name</label>
+                                <input type='text' name='username' class='input-text' />
+                                <label class='label'>Password</label>
+                                <input type='text' name='password' class='input-text' />
+                                <div class='button-row'><button type='submit' name='submit' class='submit'>Login</button></div>
+                            </form>
+                        </div>";
+                    }
+                    
+                ?>
+
             </div>
         </div>
     </div>
